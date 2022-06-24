1ec224ac57a8626a00c1c44e1a7366d601b958c1
Changmin Bae <65561206+baec23@users.noreply.github.com>
2022-06-24 16:28:22 +0900

    6/24 WIP

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,17 +3,10 @@ import logoImage from "../images/react-icon-small.png"
 
 export default function Navbar() {
     return (
-        <HeaderLeft/>
-    )
-}
-
-function HeaderLeft() {
-    return (
-        <div className="headerLeft">
-            <ul>
-                <li><img src={logoImage} alt="logoImage"/></li>
-                <li><h1>ReactFacts</h1></li>
-            </ul>
-        </div>
+        <nav className="navbar">
+            <img src={logoImage} alt="logoImage"/>
+            <h1>ReactFacts</h1>
+            <h2>React Course - Project 1</h2>
+        </nav>
     )
 }
